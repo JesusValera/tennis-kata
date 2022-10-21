123059d522432cc72ee184adb4ff86f1461436c7
JesusValera <jesus1902@outlook.com>
2022-10-21 18:44:15 +0200

    Clean code

--- php/src/TennisGame3.php
+++ php/src/TennisGame3.php
@@ -15,28 +15,51 @@ class TennisGame3 implements TennisGame
         $this->p2N = $p2N;
     }
 
-    public function getScore()
+    public function getScore(): string
     {
-        if ($this->p1 < 4 && $this->p2 < 4 && !($this->p1 + $this->p2 == 6)) {
+        if ($this->isNormalGame()) {
             $p = array("Love", "Fifteen", "Thirty", "Forty");
             $s = $p[$this->p1];
-            return ($this->p1 == $this->p2) ? "{$s}-All" : "{$s}-{$p[$this->p2]}";
-        } else {
-            if ($this->p1 == $this->p2) {
-                return "Deuce";
-            }
-            $s = $this->p1 > $this->p2 ? $this->p1N : $this->p2N;
-            return (($this->p1 - $this->p2) * ($this->p1 - $this->p2) == 1) ? "Advantage {$s}" : "Win for {$s}";
+
+            return ($this->isDeuce())
+                ? "{$s}-All"
+                : "{$s}-{$p[$this->p2]}";
         }
+
+        if ($this->isDeuce()) {
+            return "Deuce";
+        }
+
+        $s = $this->p1 > $this->p2 ? $this->p1N : $this->p2N;
+
+        if ($this->isAdvantage()) {
+            return "Advantage {$s}";
+        }
+
+        return "Win for {$s}";
     }
 
-    public function wonPoint($playerName)
+    public function wonPoint($playerName): void
     {
-        if ($playerName == "player1") {
+        if ($playerName === "player1") {
             $this->p1++;
         } else {
             $this->p2++;
         }
     }
 
+    private function isNormalGame(): bool
+    {
+        return $this->p1 < 4 && $this->p2 < 4 && !($this->p1 + $this->p2 === 6);
+    }
+
+    private function isDeuce(): bool
+    {
+        return $this->p1 == $this->p2;
+    }
+
+    private function isAdvantage(): bool
+    {
+        return ($this->p1 - $this->p2) * ($this->p1 - $this->p2) == 1;
+    }
 }
