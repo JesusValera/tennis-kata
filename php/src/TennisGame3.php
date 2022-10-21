25a0defc84025a05928a636bbbd73b03b8ec2771
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-10-21 19:14:07 +0200

    chore: extract game words to constants

--- php/src/TennisGame3.php
+++ php/src/TennisGame3.php
@@ -1,15 +1,19 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace TennisGame;
 
 class TennisGame3 implements TennisGame
 {
-    private const POINTS             = ["Love", "Fifteen", "Thirty", "Forty"];
-    private const THRESHOLD_TO_WIN   = 4;
-    const         MIN_WINNING_POINTS = 6;
-    const MIN_DIFFERENCE_OF_POINTS = 1;
+    private const POINTS = ["Love", "Fifteen", "Thirty", "Forty"];
+    private const WIN = 'Win';
+    private const THRESHOLD_TO_WIN = 4;
+    private const MIN_WINNING_POINTS = 6;
+    private const MIN_DIFFERENCE_OF_POINTS = 1;
+    const ALL = "All";
+    const DEUCE = "Deuce";
+    const ADVANTAGE = "Advantage";
     private string $playerName1;
     private string $playerName2;
 
@@ -28,12 +32,12 @@ class TennisGame3 implements TennisGame
             $s = self::POINTS[$this->playerPoints1];
 
             return $this->isDeuce()
-                ? "{$s}-All"
+                ? sprintf('%s-%s', $s, self::ALL)
                 : sprintf("%s-%s", $s, self::POINTS[$this->playerPoints2]);
         }
 
         if ($this->isDeuce()) {
-            return "Deuce";
+            return self::DEUCE;
         }
 
         $s = $this->playerPoints1 > $this->playerPoints2
@@ -41,10 +45,10 @@ class TennisGame3 implements TennisGame
             : $this->playerName2;
 
         if ($this->isAdvantage()) {
-            return "Advantage {$s}";
+            return sprintf('%s %s', self::ADVANTAGE, $s);
         }
 
-        return "Win for {$s}";
+        return sprintf('%s for %s', self::WIN, $s);
     }
 
     public function wonPoint(string $playerName): void
@@ -60,7 +64,7 @@ class TennisGame3 implements TennisGame
     private function isNormalGame(): bool
     {
         return $this->playerPoints1 < self::THRESHOLD_TO_WIN && $this->playerPoints2 < self::THRESHOLD_TO_WIN
-               && !($this->playerPoints1 + $this->playerPoints2 === self::MIN_WINNING_POINTS);
+            && !($this->playerPoints1 + $this->playerPoints2 === self::MIN_WINNING_POINTS);
     }
 
     private function isDeuce(): bool
